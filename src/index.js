1ba879f90437bd899eecca2365fdaa2ee06813b6
julianjohannesen <github email address>
2019-05-01 12:33:41 -0400

    Add redux, set up rootReducer, store, and Provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
+
 import App from './App';
+import { todosReducer } from 'src/reducers/todosReducer';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = () => {
+    return combineReducers({
+        todos: todosReducer,
+    });
+}
+
+const store = createStore(
+    rootReducer,
+    // Support for Redux dev tools
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
